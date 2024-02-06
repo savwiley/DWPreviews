e0d964581f82a4c22336181c4a975e0ef88928c2
Savannaha <savwiley@yahoo.com>
2024-02-06 17:18:41 -0600

    add dimensions to header text

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 export const Head = styled.div `
-  font: 50px var(--headerFont);
+  font: 150px var(--headerFont);
   color: var(--header);
+  cursor: default;
+  letter-spacing: 4px;
+  width: 50%;
+  padding: 20px;
+  word-break: break-all;
 `;
